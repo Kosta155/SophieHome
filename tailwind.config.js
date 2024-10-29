cc2be483edc5e04d6bc7fae1e89a5e40f7aa6ad5
Kostika Nini <ninik@sheridancollege.ca~>
2024-10-29 19:58:56 -0400

    3 try deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./assets/**/*.{js,ts,jsx,tsx}",
-    "./src/**/*.{html,js}"
-  ],
+  
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
     extend: {
         backgroundImage: {
