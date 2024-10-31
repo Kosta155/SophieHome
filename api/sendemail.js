fa5641ee19aa12a18b713ce486fe2552023a5ffb
Kostika Nini <ninik@sheridancollege.cagit config --global user.email ninik@sheridancollege.cagit config --global user.email ninik@sheridancollege.ca>
2024-10-30 23:18:54 -0400

    set up email 4

--- api/sendemail.js
+++ api/sendemail.js
@@ -1,28 +1,5 @@
-
-console.log("MAILJET_API_KEY:", process.env.MAILJET_API_KEY);
-const mailjet = require('node-mailjet').connect(process.env.MAILJET_API_KEY, process.env.MAILJET_API_SECRET);
+console.log("Jam njesh");
 
 module.exports = async (req, res) => {
-  const { from, to, subject, text } = req.body;
-  try {
-    console.log("I am here inside the handler");
-    const request = await mailjet.post('send', { version: 'v3.0' }).request({
-      "FromEmail": "kostikanini2004@gmail.com",
-      "FromName": "Your Mailjet Pilot",
-      "Recipients": [
-        {
-          "Email": "kostikanini2004@gmail.com",
-          "Name": "Passenger 1"
-        }
-      ],
-      "Subject": "Your email flight plan!",
-      "Text-part": "Dear passenger, welcome to Mailjet! May the delivery force be with you!",
-      "Html-part": "<h3>Dear passenger, welcome to Mailjet!</h3><br />May the delivery force be with you!"
-    });
-    console.log("Mailjet response:", request.body);
-    res.status(200).json({ message: 'Email sent successfully!' });
-  } catch (err) {
-    console.error('Error:', err);
-    res.status(500).json({ message: 'Failed to send the email', error: err.message });
-  }
+  res.status(200).json({ message: 'Function invoked successfully!' });
 };
