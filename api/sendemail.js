89e349d76468b5b9da12f7137d422809f5da0732
Kostika Nini <ninik@sheridancollege.cagit config --global user.email ninik@sheridancollege.cagit config --global user.email ninik@sheridancollege.ca>
2024-10-31 00:00:03 -0400

    set up email 11

--- api/sendemail.js
+++ api/sendemail.js
@@ -6,7 +6,19 @@ const mailjetClient = mailjet.apiConnect(process.env.MAILJET_API_KEY, process.en
 export default async function handler(req, res) {
 
   try {
-    const request = await mailjet.get('ping').request();
+    const request = await mailjetClient.post('send', { version: 'v3' }).request({
+      "FromEmail": "kostikanini2004@gmail.com",
+      "FromName": "Your Mailjet Pilot",
+      "Recipients": [
+        {
+          "Email": "kostikanini2004@gmail.com",
+          "Name": "Passenger 1"
+        }
+      ],
+      "Subject": "Your email flight plan!",
+      "Text-part": "Dear passenger, welcome to Mailjet! May the delivery force be with you!",
+      "Html-part": "<h3>Dear passenger, welcome to Mailjet!</h3><br />May the delivery force be with you!"
+    });
     console.log("Mailjet response:", request.body);
     res.status(200).json({ message: 'Email sent successfully!' });
   } catch (err) {
