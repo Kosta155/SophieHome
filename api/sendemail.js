19c96fe1ebc8286bc73e729bf2a1fbe5261aaa37
Kostika Nini <ninik@sheridancollege.cagit config --global user.email ninik@sheridancollege.cagit config --global user.email ninik@sheridancollege.ca>
2024-10-30 22:59:26 -0400

    set up email 4

--- api/sendemail.js
+++ api/sendemail.js
@@ -1,31 +1,28 @@
-// api/sendemail.js
 console.log("I am here");
-console.log(process.env.MAILJET_API_KEY);
+console.log("MAILJET_API_KEY:", process.env.MAILJET_API_KEY);
 const mailjet = require('node-mailjet').connect(process.env.MAILJET_API_KEY, process.env.MAILJET_API_SECRET);
 
 module.exports = async (req, res) => {
   const { from, to, subject, text } = req.body;
   try {
-    console.log("I am here");
-    const request = mailjet.post('send', { version: 'v3.0' }).request({
-      "FromEmail":"kostikanini2004@gmail.com",
-      "FromName":"Your Mailjet Pilot",
-      "Recipients":[
+    console.log("I am here inside the handler");
+    const request = await mailjet.post('send', { version: 'v3.0' }).request({
+      "FromEmail": "kostikanini2004@gmail.com",
+      "FromName": "Your Mailjet Pilot",
+      "Recipients": [
         {
-          "Email":"kostikanini2004@gmail.com",
-          "Name":"Passenger 1"
+          "Email": "kostikanini2004@gmail.com",
+          "Name": "Passenger 1"
         }
       ],
-      "Subject":"Your email flight plan!",
-      "Text-part":"Dear passenger, welcome to Mailjet! May the delivery force be with you!",
-      "Html-part":"<h3>Dear passenger, welcome to Mailjet!</h3><br />May the delivery force be with you!"
-    })
-	.then((result) => {
-		console.log(result.body)
-	});
+      "Subject": "Your email flight plan!",
+      "Text-part": "Dear passenger, welcome to Mailjet! May the delivery force be with you!",
+      "Html-part": "<h3>Dear passenger, welcome to Mailjet!</h3><br />May the delivery force be with you!"
+    });
+    console.log("Mailjet response:", request.body);
     res.status(200).json({ message: 'Email sent successfully!' });
   } catch (err) {
-    console.error(err.statusCode);
+    console.error('Error:', err);
     res.status(500).json({ message: 'Failed to send email', error: err.message });
   }
 };
